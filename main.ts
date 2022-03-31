41a951b2b5f0f67dd525d108a2f66a78cd639aed
RJWPod <101339801+RJWPod@users.noreply.github.com>
2022-03-31 09:50:30 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+let P1_WinsPoints = 0
+let P2_WinsPoints = 0
+let Coin_1 = 0
+let Coin_2 = 0
 input.onButtonPressed(Button.AB, function () {
     basic.showString("P1")
     basic.showNumber(P1_WinsPoints)
@@ -17,7 +21,7 @@ input.onGesture(Gesture.Shake, function () {
         basic.showString("2t")
         P1_WinsPoints += 1
         basic.showString("P1Wins")
-    } else if (Coin_1 == 1 && (Coin_2 == 0 || Coin_2 == 0 && Coin_1 == 1)) {
+    } else if (!(Coin_1 == 0 && Coin_2 == 0 || Coin_1 == 1 && Coin_2 == 1)) {
         basic.showString("1h,1t")
         basic.showString("P2Wins")
         P2_WinsPoints += 1
@@ -25,16 +29,3 @@ input.onGesture(Gesture.Shake, function () {
     Coin_1 = 2
     Coin_2 = 2
 })
-let Coin_2 = 0
-let Coin_1 = 0
-let P2_WinsPoints = 0
-let P1_WinsPoints = 0
-basic.showLeds(`
-    . # # # .
-    # . . . #
-    # . . . #
-    # . . . #
-    . # # # .
-    `)
-basic.showString("h=heads")
-basic.showString("t=tails")
